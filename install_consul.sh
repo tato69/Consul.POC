783cd133e2d9e98af909e4b585b345addc1d7673
tato69 <ballacoipruppi@hotmail.com>
2018-03-08 14:56:30 +0100

    Update install_consul.sh
    corrected unzip command

--- install_consul.sh
+++ install_consul.sh
@@ -2,4 +2,4 @@
 apt-get update 
 apt-get install unzip
 wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip?_ga=2.264617996.228682823.1520510943-1002964709.1520510943 -O /tmp/consul.zip
-unzip -d /tmp/consul.zip -d /usr/local/sbin/
+unzip -d /tmp/consul.zip /tmp/consul.zip
